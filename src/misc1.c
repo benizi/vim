207fcb168938e83d3946d23fdec928ee48ef08a1
Benjamin R. Haskell <vim@benizi.com>
2017-10-17 00:25:35 -0400

    Revert patch 8.0.0851
    Patch 8.0.0851: 'smartindent' is used even when 'indentexpr' is set
    Commit: 69a76feda9e9d308be6b5fc2185286a061dfecd6
    
    This breaks benizi/vim-endwise for `&ft == "sh"`. Probably just need to
    upgrade it, but just pushing this up to GitHub for the moment while
    diagnosing the issue.
    
    Example breakage, with `▏` standing in for the cursor position.
    
    Starting from:
    
        #!/bin/sh
        if true▏
    
    With the patch, hitting `<CR>` indents the following `fi` too far:
    
        #!/bin/sh
        if true
          ▏
          fi
    
    With the patch reverted, it works as expected:
    
        #!/bin/sh
        if true
          ▏
        fi

--- src/misc1.c
+++ src/misc1.c
@@ -627,9 +627,6 @@ open_line(
 # ifdef FEAT_CINDENT
 					&& !curbuf->b_p_cin
 # endif
-# ifdef FEAT_EVAL
-					&& *curbuf->b_p_inde == NUL
-# endif
 			);
     int		no_si = FALSE;		/* reset did_si afterwards */
     int		first_char = NUL;	/* init for GCC */
